1442b624fb576e001e6ded76f6adfee4b486438d
mryanshenghong <mryanshenghong@gmail.com>
2024-07-26 22:01:21 -0400

    test addon UI

--- testAddon/test.lua
+++ testAddon/test.lua
@@ -1,6 +1,30 @@
-local frame = CreateFrame('Frame')
+-- 创建一个框架
+local frame = CreateFrame("Frame", "MyFirstAddonFrame", UIParent)
+frame:SetSize(200, 100)  -- 设置框架大小
+frame:SetPoint("CENTER") -- 设置框架位置为屏幕中央
 
+-- 添加一个背景颜色
+frame:SetBackdrop({
+    bgFile = "Interface/Tooltips/UI-Tooltip-Background",
+    edgeFile = "Interface/Tooltips/UI-Tooltip-Border",
+    tile = true,
+    tileSize = 16,
+    edgeSize = 16,
+    insets = { left = 4, right = 4, top = 4, bottom = 4 }
+})
+frame:SetBackdropColor(0, 0, 0, 1) -- 背景颜色：黑色
+
+-- 创建一个文本标签
+local text = frame:CreateFontString(nil, "OVERLAY", "GameFontNormal")
+text:SetPoint("CENTER")                    -- 设置文本位置为框架中央
+text:SetText("Welcome to My First Addon!") -- 设置文本内容
+
+-- 注册一个事件
 frame:RegisterEvent("PLAYER_LOGIN")
+
+-- 定义事件处理函数
 frame:SetScript("OnEvent", function(self, event, ...)
-    print("test")
+    if event == "PLAYER_LOGIN" then
+        print("My First Addon loaded!")
+    end
 end)
